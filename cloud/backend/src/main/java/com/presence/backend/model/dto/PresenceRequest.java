101226f2dee7d9ab35c8c66e4e6548a024006d30
isilva-t <ivanrfteixeira@gmail.com>
2025-12-08 19:30:33 +0000

    PresenceRequest

---
+++ cloud/backend/src/main/java/com/presence/backend/model/dto/PresenceRequest.java
@@ -0,0 +1,23 @@
+package com.presence.backend.model.dto
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class PresenceRequest {
+	private String siteId;
+	private String timestamp;
+	private List<PresenceData> presenceData;
+
+	@Data
+	public static class PresenceData {
+		private Integer	employeeId;
+		private String	employeeName;
+		private String	fakeName;
+		private String	date;
+		private Integer	hour;
+		private String	firstSeen;
+		private String	lastSeen;
+		private Integer	minutesOnline;
+	}
+}
