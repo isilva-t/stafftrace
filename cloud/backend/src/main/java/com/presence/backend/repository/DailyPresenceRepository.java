4204384ae0a80849b2a88e1fd6fcffa656ce9ebe
isilva-t <ivanrfteixeira@gmail.com>
2025-12-08 19:43:54 +0000

    DailyPresenceRepository

---
+++ cloud/backend/src/main/java/com/presence/backend/repository/DailyPresenceRepository.java
@@ -0,0 +1,14 @@
+package com.presence.backend.repository;
+
+import com.presence.backend.model.DailyPresence;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface DailyPresenceRepository extends MongoRepository<DailyPresence, String> {
+	Optional<DailyPresence> findByEmployeeIdAndDate(Integer employeeId, LocalDate date);
+	List<DailyPresence> findByDate(LocalDate date);
+	List<DailyPresence> findByDateBetween(LocalDate startDate, LocalDate endDate);
+	List<DailyPresence> findByEmployeeIdAndDateBetween(Integer employeeId, Localdate startDate, LocalDate endDate);
+}
