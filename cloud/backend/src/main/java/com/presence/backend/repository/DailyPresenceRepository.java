e83fa0de254beebc9331b8d5ca9a48f9d8ddf927
isilva-t <ivanrfteixeira@gmail.com>
2025-12-08 23:15:08 +0000

    fix: typos dailyprecencerepository

--- cloud/backend/src/main/java/com/presence/backend/repository/DailyPresenceRepository.java
+++ cloud/backend/src/main/java/com/presence/backend/repository/DailyPresenceRepository.java
@@ -2,6 +2,7 @@ package com.presence.backend.repository;
 
 import com.presence.backend.model.DailyPresence;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +11,5 @@ public interface DailyPresenceRepository extends MongoRepository<DailyPresence, 
 	Optional<DailyPresence> findByEmployeeIdAndDate(Integer employeeId, LocalDate date);
 	List<DailyPresence> findByDate(LocalDate date);
 	List<DailyPresence> findByDateBetween(LocalDate startDate, LocalDate endDate);
-	List<DailyPresence> findByEmployeeIdAndDateBetween(Integer employeeId, Localdate startDate, LocalDate endDate);
+	List<DailyPresence> findByEmployeeIdAndDateBetween(Integer employeeId, LocalDate startDate, LocalDate endDate);
 }
