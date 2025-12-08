887ceb659bc456bfcac0bce2b40c8f5659e1c3e6
isilva-t <ivanrfteixeira@gmail.com>
2025-12-08 23:15:22 +0000

    fix: typos currentstatusrepository

--- cloud/backend/src/main/java/com/presence/backend/repository/CurrentStatusRepository.java
+++ cloud/backend/src/main/java/com/presence/backend/repository/CurrentStatusRepository.java
@@ -5,6 +5,6 @@ import com.presence.backend.model.CurrentStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
-public interface CurrentStatusRepository extends MongoRepository<CurrentStatus, Strign> {
+public interface CurrentStatusRepository extends MongoRepository<CurrentStatus, String> {
 	Optional<CurrentStatus> findByEmployeeId(Integer employeeId);
 }
