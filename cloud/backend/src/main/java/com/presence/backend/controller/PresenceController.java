6a36580651ca3d4e9b89b779adf97278ce89a9e8
isilva-t <ivanrfteixeira@gmail.com>
2025-12-08 21:19:16 +0000

    PresenceController post presence

--- cloud/backend/src/main/java/com/presence/backend/controller/PresenceController.java
+++ cloud/backend/src/main/java/com/presence/backend/controller/PresenceController.java
@@ -34,4 +34,49 @@ public class PresenceController {
 	@Autowired
 	private NameMappingService nameMappingService;
 
+	@PostMapping("/presence")
+	public ResponseEntity<String> receivePresence(@RequestBody PresenceRequest request) {
+		
+		for (PresenceRequest.PresenceData data : request.getPresenceData()) {
+			LocalDate date = LocalDate.parse(data.getDate());
+
+			//find or create daily presence record
+			DailyPresence daily = dailyPresenceRepository
+								.findByEmployeeInAndDate(data.getEmployeeId(), date)
+								.orElse(new DailyPresence());
+
+			boolean isNew = daily.getId() == null;
+
+			daily.setEmployeeId(data.getEmployeeId());
+			daily.setEmployeeName(data.getEmployeeName());
+			daily.setFakeName(data.getFakeName());
+			daily.setDate(date);
+
+			LocalTime newFirstSeen = LocalTime.parse(data.getFirstSeen());
+			if (isNew || daily.getFirstSeen() == null
+				|| newFirstSeen.isBefore(daily.getFirstSeen())) {
+				daily.setFirstSeen(newFirstSeen);
+			}
+			
+			LocalTime newLastSeen = LocalTime.parse(data.getLastSeen());
+			if (isNew || daily.getLastSeen() == null
+				|| newLastSeen.isAfter(daily.getLastSeen()) {
+				daily.setLastSeen(newLastSeen);
+			}
+
+			int currentMinutes = daily.getTotalMinutes() != null ? daily.getTotalMinutes() : 0;
+			daily.setTotalMinutes(currentMinutes + data.getMinutesOnline());
+			daily.setHoursPresent(daily.getTotalMinutes() / 60.0);
+
+			// set timestamps
+			if (isNew) {
+				daily.setCreatedAt(LocalDateTime.now());
+			}
+			daily.setUpdatedAt(LocalDateTime.now());
+
+			dailyPresenceRepository.save(daily);
+		}
+		return ResponseEntity.ok("Presence data received");
+	}
+
 }
