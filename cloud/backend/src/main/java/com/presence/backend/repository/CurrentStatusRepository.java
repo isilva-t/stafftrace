f9b7a02f2b71759bbb96fd58828d95ad7d0675f3
isilva-t <ivanrfteixeira@gmail.com>
2025-12-08 19:38:07 +0000

    CurrentStatusRepository

---
+++ cloud/backend/src/main/java/com/presence/backend/repository/CurrentStatusRepository.java
@@ -0,0 +1,10 @@
+package com.presence.backend.repository;
+
+import com.presence.backend.model.CurrentStatus;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface CurrentStatusRepository extends MongoRepository<CurrentStatus, Strign> {
+	Optional<CurrentStatus> findByEmployeeId(Integer employeeId);
+}
