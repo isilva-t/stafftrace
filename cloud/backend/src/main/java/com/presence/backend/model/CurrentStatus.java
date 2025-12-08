417ca5c0ea1ff5f88e08efe54f5cf334a2b6cbbb
isilva-t <ivanrfteixeira@gmail.com>
2025-12-08 18:25:46 +0000

    CurrentStatus and DailyPresence

---
+++ cloud/backend/src/main/java/com/presence/backend/model/CurrentStatus.java
@@ -0,0 +1,21 @@
+package com.presence.backend.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "current_status")
+public class CurrentStatus {
+	@Id
+	private String id;
+
+	private Integer			employeeId;
+	private String			employeeName;
+	private String			fakeName;
+	private Boolean			isPresent;
+	private String			currentArea;
+	private LocalDateTime	lastSeen;
+	private	LocalDateTime	updateAt;
+}
---
+++ cloud/backend/src/main/java/com/presence/backend/model/DailyPresence.java
@@ -0,0 +1,28 @@
+package com.presence.backend.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@Document(collection = "daily_presence")
+@CompoundIndex(name = "employee_date_idx", def = "{'employeeId': 1, 'date': 1}", unique = true}
+public class DailyPresence {
+	@Id
+	private String id;
+
+	private Integer employeeId;
+	private String employeeName;
+	private String fakeName;
+	private LocalDate date;
+	private LocalTime firstSeen;
+	private LocalTime lastSeen;
+	private Integer totalMinutes;
+	private Double hoursPresent;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+}
