6bc0bd0869748ffc0f17bd9c5c5192e1e35f1591
isilva-t <ivanrfteixeira@gmail.com>
2025-12-09 12:12:33 +0000

    fix: timezone

--- cloud/frontend/src/app/app.ts
+++ cloud/frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CurrentStatus } from './components/current-status/current-status';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CurrentStatus],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  title = 'frontend'
 }
