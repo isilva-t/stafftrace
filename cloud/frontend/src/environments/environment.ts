e3db502091eb2f9a906a86b2deaec7e4b0587fad
isilva-t <ivanrfteixeira@gmail.com>
2025-12-10 10:29:59 +0000

    feat: containerized frontend
    have two options based on env variable
    could be
    Dockerfile.dev
    or
    Dockerfile , for production

--- cloud/frontend/src/environments/environment.ts
+++ cloud/frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'http://localhost:8080/api', // this variable is changed in build time,
+  // on production to '/api'. check Dockerfile and see sed
   refreshInterval: 15000,
   locale: 'en-GB'
 };
